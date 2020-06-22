ea630377799fc50fe000eead917cf110bdd56bcd
fedestrife <federc.prc@gmail.com>
2020-06-22 17:22:40 +0200

    Terzo Aggiornamento
    Ho potuto lavorarci solo ieri e oggi

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+  // Quando clicco ne tasto Invio(send-button)
+  $('.send-button').click(function() {
+
+  });
+  //
+  function sendMessages() {
+    var messageText = $('.send-messages-input input').val();
+    if(messagetext) != '') {
+      var newMessage = $('.templates .message').clone();
+    }
+  }
+});
